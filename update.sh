d95b959b6015c67c1e0509d590b53fee65bfb11a
Sujeevraja Sanjeevi <6365621+sujeevraja@users.noreply.github.com>
2015-12-23 00:06:50 -0600

    renamed .vim to vim.

--- update.sh
+++ update.sh
@@ -9,24 +9,24 @@ git pull
 cd $BASE_DIR
 
 # Install pathogen
-mkdir -p .vim .vim/autoload .vim/bundle
-cp $PWD/vim-pathogen/autoload/pathogen.vim $PWD/.vim/autoload
+mkdir -p vim vim/autoload vim/bundle
+cp $PWD/vim-pathogen/autoload/pathogen.vim $PWD/vim/autoload
 echo "copied pathogen.vim."
 
 # Update darcula
-cd .vim/bundle/darcula
+cd vim/bundle/darcula
 echo "updating darcula."
 git pull
 cd $BASE_DIR
 
 # Update nerdcommenter
-cd .vim/bundle/nerdcommenter
+cd vim/bundle/nerdcommenter
 echo "updating nerdcommenter."
 git pull
 cd $BASE_DIR
 
 # Update syntastic
-cd .vim/bundle/syntastic
+cd vim/bundle/syntastic
 echo "updating syntastic."
 git pull
 cd $BASE_DIR
