e9e1aebabd42149eb799232876a4e897512b462c
Sujeevraja Sanjeevi <6365621+sujeevraja@users.noreply.github.com>
2015-12-23 00:26:41 -0600

    cleaned up update script.

--- update.sh
+++ update.sh
@@ -2,31 +2,9 @@
 
 BASE_DIR=$PWD
 
-# Update pathogen submodule
-cd vim-pathogen
-echo "updating pathogen."
-git pull
-cd $BASE_DIR
+git submodule foreach git pull origin master
 
 # Install pathogen
 mkdir -p vim vim/autoload vim/bundle
 cp $PWD/vim-pathogen/autoload/pathogen.vim $PWD/vim/autoload
 echo "copied pathogen.vim."
-
-# Update darcula
-cd vim/bundle/darcula
-echo "updating darcula."
-git pull
-cd $BASE_DIR
-
-# Update nerdcommenter
-cd vim/bundle/nerdcommenter
-echo "updating nerdcommenter."
-git pull
-cd $BASE_DIR
-
-# Update syntastic
-cd vim/bundle/syntastic
-echo "updating syntastic."
-git pull
-cd $BASE_DIR
