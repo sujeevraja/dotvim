f9b6ee03f8c726f8d111b179610a297ac6628b47
Sujeevraja Sanjeevi <6365621+sujeevraja@users.noreply.github.com>
2025-05-28 17:42:16 -0700

    update linux-copy script

--- linux_copy.sh
+++ linux_copy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-rm -rf ~/.vim .vimrc
+rm -rf ~/.vim
+echo "removed ~/.vim"
+
 cp -r vim ~/.vim
+echo "copied vim folder to ~/.vim"
+
 cp vimrc ~/.vimrc
+echo "copied vimrc to ~/.vimrc"
+
+echo "completed copying"
